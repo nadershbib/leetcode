69db73c423e001600c7851cc5e0c5902312cea58
nadershbib <nader_shbib@hotmail.com>
2026-07-30 06:14:42 -0700

    slain

---
+++ easy/20_validParentheses.py
@@ -0,0 +1,56 @@
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         open = {"(","[","{"}
+#         close = {")","]","}"}
+#         valid = {"()","{}","[]"}
+#         stack = []
+#         for c in s:
+#             if not stack:
+#                 if c in open:
+#                     stack.append(c)
+#                     continue
+#                 else:
+#                     return False
+            
+#             if stack[-1] in open and c in close:
+#                 if stack[-1]+c in valid:
+#                     stack.pop()
+#                     continue
+#                 else:
+#                     return False
+#             stack.append(c)
+#         if not stack:
+#             return True
+#         return False
+
+
+# print(Solution().isValid("()[]{}"))
+
+
+# optimized solution/ using dictionary for the pairs
+# solved 7 / 30 / 2026
+class Solution:
+    def isValid(self,s:str) -> bool:
+        pairs = {
+            "(":")",
+            "{":"}",
+            "[":"]"
+        }
+        stack = []
+        for c in s:
+            if not stack:
+                if c in pairs:
+                    stack.append(c)
+                    continue
+                return False
+            
+            if stack[-1] in pairs:
+                if c in pairs:
+                    stack.append(c)
+                elif c == pairs[stack[-1]]:
+                    stack.pop()
+                else:
+                    return False
+        return not stack
+             
+            
