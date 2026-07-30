7cb9b26d823f4bf63eb816a1e0028b62b2ffd5a5
nadershbib <nader_shbib@hotmail.com>
2026-07-30 03:58:05 -0700

    slain

--- easy/14_longestCommonPrefix.py
+++ easy/14_longestCommonPrefix.py
@@ -14,3 +14,7 @@ print(Solution().longestCommonPrefix(["dog","racecar","car"]))
 # O(n*m) since shortest word length (n) is different than the number of strings left to check (m) -> n*m
 
 # min(strs,key = len) to get shortest word in python 
+
+
+# the top solution is basically sort them list of strings, compare first and last word characters if they're matching everthing else in between should be matching, since when you sort list of strings alphabetically everything should be close to each other to the other words, so taking first and last word are the furthest from each other from alphabetical order hence you should compare those 2, if there's a mismatch you terminate, if not all words in between would fucking align when it comes to the prefix.
+
