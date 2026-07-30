5fdbd88a89d64515cb5ece518177f37f887eef5d
nadershbib <nader_shbib@hotmail.com>
2026-07-30 03:53:02 -0700

    slain

--- easy/14_longestCommonPrefix.py
+++ easy/14_longestCommonPrefix.py
@@ -1,18 +1,16 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         commonPrefix = ""
-        shortestWord = min(strs,key=len)
-        for i,c in enumerate(shortestWord):
+        ShortestWord = min(strs,key=len)
+        for i,c in enumerate(ShortestWord):
             for j in range(len(strs)):
-                if c == strs[j][i]:
-                    continue
-                else:
+                if c != strs[j][i]:
                     return commonPrefix
             commonPrefix+=c
         return commonPrefix
 
 print(Solution().longestCommonPrefix(["dog","racecar","car"]))
 
-# O(n*m) checking common prefix accross all strings, it is O(n*m) since it could be terminated as soon as we find something that is not common
+# O(n*m) since shortest word length (n) is different than the number of strings left to check (m) -> n*m
 
 # min(strs,key = len) to get shortest word in python 
